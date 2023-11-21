98f1446ad79d8bd20dc0265055f984f35292aba4
JAIN <aman.jain1@hp.com>
2023-11-22 02:38:07 +0530

    two sum push

---
+++ coding/python/two_sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        result = []
+        for i in range(0, len(nums)):
+            for j in range(i+1,len(nums)):
+                if (target == nums[i]+nums[j]):
+                    result.append(i)
+                    result.append(j)
+                    return result
+
+        return result
