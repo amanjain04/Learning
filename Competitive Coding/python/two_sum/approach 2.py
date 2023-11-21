cb946351e85fd3715c73311f1c09f4df428e98f8
aman jain <amanjain0314@gmail.com>
2023-11-22 03:55:53 +0530

    new

--- Competitive Coding/python/two_sum/approach 2.py
+++ Competitive Coding/python/two_sum/approach 2.py
@@ -1,2 +1,13 @@
-## ad barnch
-asas
+Time Complexity : O(N)
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        result = {}
+        for i in range(0, len(nums)):
+            diff = target - nums[i]
+
+            if diff not in result:
+                result[nums[i]]=i
+
+            else:
+                return [result[diff],i]
