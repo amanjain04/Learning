03157ac73cbcfdfcd3298330ee5a1f852f5f70b7
aman jain <amanjain0314@gmail.com>
2023-12-25 03:38:33 +0530

    add

--- Competitive Coding/python.py
+++ Competitive Coding/python.py
@@ -60,18 +60,98 @@
 # print(a[::-1])
 #string are immutable
 #example 2
-a = "amanjain"
-print(a.upper())
-print(a.lower())
-print(a.replace("a","b"))
-print(a.split(" ")) # it will split the string from the space   and return a list
-print(a.split("a")) # it will split the string from the a   and return a list
-print(a.find("a")) # it will return the index of the first occurence of the a
-print(a.count("a")) # it will return the count of the a in the string
-print(a.isalpha()) # it will return true if the string is alpha else return false
-print(a.isalnum()) # it will return true if the string is alpha numeric else return false
-print(a.isnumeric()) # it will return true if the string is numeric else return false
+# a = "amanjain"
+# print(a.upper())
+# print(a.lower())
+# print(a.replace("a","b"))
+# print(a.split(" ")) # it will split the string from the space   and return a list
+# print(a.split("a")) # it will split the string from the a   and return a list
+# print(a.find("a")) # it will return the index of the first occurence of the a
+# print(a.count("a")) # it will return the count of the a in the string
+# print(a.isalpha()) # it will return true if the string is alpha else return false
+# print(a.isalnum()) # it will return true if the string is alpha numeric else return false
+# print(a.isnumeric()) # it will return true if the string is numeric else return false
 
-print(a.rstrip(2)) # it will remove the space from the right side of the string
+# print(a.rstrip(2)) # it will remove the space from the right side of the string
 
 
+## case
+# x = 50
+# match x:
+#     case 1:
+#         print("one")
+#     case 2:
+#         print("two")
+#     case 3:
+#         print("three")
+#     case 4:
+#         print("four")
+#     case 5:
+#         print("five")
+#     case _:
+#         print("something else")
+
+#functions
+# def add(a,b):
+#     return a+b
+# print(add(2,3))
+
+# def name(fname, mname ="kumar", lname="jain"):
+#     print("hello " ,fname,mname,lname)
+# name("aman")
+
+
+# def add(*args):
+#     print(args)
+
+# add(1,2,3,4,5,6,7,8,9,10)
+
+# def add(**args):
+#     print(args)
+# add(a=1,b=2,c=3,d=4,e=5,f=6,g=7,h=8,i=9,j=10)
+
+
+##list are ordered and mutable
+# list1 = [1,2,3,4,5,6,7,8,9,10]
+# print(list1)
+# print(list1[0])
+# list1.append(11)
+# list1.insert(0,0)
+# list1.remove(0)
+# list1.sort()
+# list1.reverse()
+# list2 = list1.copy() # it will copy the list1 to list2
+# list1.extend(list2) # it will extend the list1 with the list2
+# print(list1)
+
+
+# #tuple are ordered and immutable
+# tuple1 = (1,2,3,4,5,6,7,8,9,10)
+# print(tuple1)
+# print(tuple1[0])
+
+
+##string formating
+# fname = "aman"
+# lname = "jain"
+# print("hello {} {}".format(fname,lname))
+
+
+# print(f"hello {fname} {lname}")
+
+
+##doc string
+# def add(a,b):
+#     """this function will add two number"""
+#     return a+b
+# a = add().__doc__
+# print(a)
+
+
+## set are unordered and unindexed, no duplicate value
+set1 = {"carla",19 , False , 5.9}
+print(set1)
+set1.add(11)
+set1.remove(11)
+set1.discard(11)
+set1.pop()
