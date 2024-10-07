01fa96237ca83a78dcc8120f5ab54cc2648deb2d
Aman Jain <aman.jain2@bp.com>
2024-10-07 21:59:37 +0530

    update approch 2 in two sum

--- Competitive Coding/python/1. Two Sum/approach 2.py
+++ Competitive Coding/python/1. Two Sum/approach 2.py
@@ -1,13 +1,11 @@
-#Time Complexity : O(N)
-
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        result = {}
-        for i in range(0, len(nums)):
+        seen = {}
+        for i in range(len(nums)):
             diff = target - nums[i]
-
-            if diff not in result:
-                result[nums[i]]=i
-
+            if diff in seen:
+                return [seen[diff],i]
             else:
-                return [result[diff],i]
+                seen[nums[i]]=i
+
+        return 
