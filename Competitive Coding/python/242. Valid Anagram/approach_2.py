6d83107ca4f52388cfe34b40b09db750635f6203
Aman Jain <amanjain0314@gmail.com>
2025-02-23 01:15:43 +0530

    1

--- Competitive Coding/python/242. Valid Anagram/approach_2.py
+++ Competitive Coding/python/242. Valid Anagram/approach_2.py
@@ -2,14 +2,14 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if (len(s) != len(t)):
             return False
-        a = dict()
+        a = {}
         for i in s:
             a[i] = a.get(i,0)+1
         for j in t:
             if j in a:
-                a[j] = a.get(j,0)-1
+                a[j] -= 1
             else:
-                False
+                return False
 
         for k,v in a.items():
             if (v!=0):
