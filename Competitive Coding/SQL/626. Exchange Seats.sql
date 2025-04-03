4634d9c1245a10c04b3d6c9161cfd600e9a698bd
Aman Jain <amanjain0314@gmail.com>
2025-04-03 20:46:31 +0530

    1

---
+++ Competitive Coding/SQL/626. Exchange Seats.sql
@@ -0,0 +1,77 @@
+Table: Seat
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| student     | varchar |
++-------------+---------+
+id is the primary key (unique value) column for this table.
+Each row of this table indicates the name and the ID of a student.
+The ID sequence always starts from 1 and increments continuously.
+ 
+
+Write a solution to swap the seat id of every two consecutive students. If the number of students is odd, the id of the last student is not swapped.
+
+Return the result table ordered by id in ascending order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Seat table:
++----+---------+
+| id | student |
++----+---------+
+| 1  | Abbot   |
+| 2  | Doris   |
+| 3  | Emerson |
+| 4  | Green   |
+| 5  | Jeames  |
++----+---------+
+Output: 
++----+---------+
+| id | student |
++----+---------+
+| 1  | Doris   |
+| 2  | Abbot   |
+| 3  | Green   |
+| 4  | Emerson |
+| 5  | Jeames  |
++----+---------+
+Explanation: 
+Note that if the number of students is odd, there is no need to change the last one's seat.
+
+Solutions:
+
+1.
+
+SELECT (CASE WHEN MOD(id,2)=1 AND id!=(SELECT COUNT(*) FROM seat) THEN id+1
+WHEN MOD(id,2)=0 THEN id-1
+ELSE id END)id, student
+FROM seat
+ORDER BY 1
+
+2. 
+
+SELECT 
+     ID,
+     IFNULL(CASE WHEN id % 2 != 0 
+                 THEN lead(student) over ()  
+                 ELSE lag(student) over ()
+            END, student
+            ) AS student
+     FROM seat;
+
+3.
+
+SELECT
+    s1.id, COALESCE(s2.student, s1.student) AS student
+FROM
+    seat s1
+        LEFT JOIN
+    seat s2 ON ((s1.id + 1) ^ 1) - 1 = s2.id
+ORDER BY s1.id;
