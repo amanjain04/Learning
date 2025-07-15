26bac4b52beb49b27a144cf71fb058500396bf57
Aman Jain <amanjain0314@gmail.com>
2025-07-16 02:16:31 +0530

    1

--- Competitive Coding/SQL/626. Exchange Seats.sql
+++ Competitive Coding/SQL/626. Exchange Seats.sql
@@ -49,8 +49,8 @@ Solutions:
 
 1.
 
-SELECT (CASE WHEN MOD(id,2)=1 AND id!=(SELECT COUNT(*) FROM seat) THEN id+1
-WHEN MOD(id,2)=0 THEN id-1
+SELECT (CASE WHEN id%2=1 AND id!=(SELECT COUNT(*) FROM seat) THEN id+1
+WHEN id%2=0 THEN id-1
 ELSE id END)id, student
 FROM seat
 ORDER BY 1
